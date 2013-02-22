d7e2a2ae49f63f6803f78a91a12c0b8cc4bc2ea3
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-02-22 11:44:12 -0800

    Enabling random computer moves

--- player.py
+++ player.py
@@ -1,21 +1,22 @@
+import random
+
 class Player:
     def __init__(self, color, interface):
         self.color = color
         self.interface = interface
 
-    def getMove(self):
+    def getMove(self, board=None):
         raise NotImplementedError
 
 class Computer(Player):
     def __init__(self, color, interface):
         super().__init__(color, interface)
-        raise NotImplementedError
 
-    def getMove(self):
-        raise NotImplementedError
+    def getMove(self, board):
+        return random.randint(0, board.width)
 
 class Human(Player):
     def __init__(self, color, interface):
         super().__init__(color, interface)
-    def getMove(self):
+    def getMove(self, board=None):
         return self.interface.askMove(self.color)
