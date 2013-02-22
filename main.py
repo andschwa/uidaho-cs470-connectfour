7faac256d55fa99b0514380160d016dd14fc2e8b
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-02-21 20:05:30 -0800

    Implementing options parser

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ import game
 import interface
 
 def eatargs():
-    parser = argparse
+    parser = argparse.ArgumentParser(description='Play Connect Four!')
+    parser.add_argument('--versus', dest='players', action='store_const',
+            const=['human', 'human'], help='Play as two humans!')
+    parser.add_argument('--solo', dest='players', action='store_const',
+            const=['human', 'computer'], help='Play as two humans!')
+    parser.add_argument('--auto', dest='players', action='store_const',
+            const=['computer', 'computer'], help='Play as two computers!')
 
 def main():
     options, args = eatargs()
@@ -18,6 +24,9 @@ def main():
         the_interface = interface.cli()
 
     the_game = game(the_interface, options.players)
+
+    the_game.setup()
+    the_game.play()
 
 if __name__ = '__main__':
     sys.exit(main())
