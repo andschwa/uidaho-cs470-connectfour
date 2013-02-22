d08b6fbbd04abc7b5bd953358a00030b6378bf0a
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-02-21 23:23:53 -0800

    Adding play method

--- game.py
+++ game.py
@@ -1,20 +1,45 @@
-import board.Board as Board
-import player.Human as Human
-import player.Computer as Computer
+from board import Board
+from error import *
+from player import Human, Computer
 
 class Game:
-    def __init__(interface, players):
+    def __init__(self, interface, players):
         self.interface = interface
         self.playing = True
-        self.board = Board()
+        self.winnder = None
+        self.board = Board(self.interface)
 
         self.players = [
                 Human('red', self.interface),
                 Human('black', self.interface)]
 
-    def play():
+    def newGame(self):
+        raise NotImplementedError
+
+    def _move(self, player):
+        try:
+            column = player.getMove()
+            move = self.board.makeMove(column, player.color)
+        except InvalidMoveError:
+            return None
+        return move
+
+    def play(self):
+        self.board.printBoard()
         while self.playing:
-            for player in players:
-                move = player.getMove()
-                self.board.makeMove(move)
+            for player in self.players:
+                move = None
+                while move is None:
+                    move = self._move(player)
+                print('Got move {}'.format(move))
+                self.board.printBoard()
+
+                if self.board.moveWon(move):
+                    self.playing = False
+                    self.winner = player
+        self.endGame()
+
+    def endGame(self):
+        print('Player {} won!'.format(self.winner.color))
+        self.board.printBoard()
 
